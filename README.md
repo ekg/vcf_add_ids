5308ab0680f99787a08b748d631eb86c3a6560a0
Erik Garrison <erik.garrison@gmail.com>
2023-07-24 13:03:27 +0200

    more options, tested even

---
+++ README.md
@@ -0,0 +1,8 @@
+# vcf id adder
+
+VCF id can be useful for processing.
+
+This is a quick script that assigns IDs in a VCF file based on the position and alleles.
+
+The delimiter can be set.
+Optionally, a sha1 hash can be use to get an id, which can be more compact.
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use flate2::read::MultiGzDecoder;
 use std::fs::File;
 use std::io::{BufReader, BufRead, stdout, Write};
 use clap::Parser;
+use sha1::{Sha1, Digest};
 
 #[derive(Parser)]
 #[command(name = "VCF ID adder")]
@@ -12,11 +13,19 @@ use clap::Parser;
 struct Cli {
     #[arg(short,long)]
     input: String,
+    #[arg(short,long)]
+    sha1_hash: bool, // use a hash value instead of the position and alleles
+    #[arg(short,long,default_value = "_")]
+    delim: String, // delimiter for the ID field
 }
 
 fn main() -> Result<(), VCFError> {
 
     let cli = Cli::parse();
+
+    let use_hash_id = cli.sha1_hash;
+
+    let delim = cli.delim;
 
     // copy cli.input into filenam
     let filename = cli.input;
@@ -34,13 +43,19 @@ fn main() -> Result<(), VCFError> {
     // prepare VCFRecord object
     let mut vcf_record = reader.empty_record();
 
-    while reader.next_record(&mut vcf_record).is_ok() {
+    while reader.next_record(&mut vcf_record)? {
         let chrom = std::str::from_utf8(&vcf_record.chromosome)?;
         let pos = vcf_record.position.to_string();
         // as a string
         let alternative = std::str::from_utf8(vcf_record.alternative.first().unwrap())?.to_string();
         let reference = std::str::from_utf8(&vcf_record.reference)?.to_string();
-        let new_id = format!("{}:{}:{}:{}", chrom, pos, reference, alternative);
+        let mut new_id = format!("{}{}{}{}{}{}{}", chrom, delim, pos, delim, reference, delim, alternative);
+        if use_hash_id {
+            let mut hasher = Sha1::new();
+            hasher.update(new_id.as_bytes());
+            // take id as a string that's the first 4 bytes of the hash
+            new_id = hasher.finalize().iter().take(4).map(|b| format!("{:02x}", b)).collect::<String>();
+        }
         vcf_record.id = vec![U8Vec::from(new_id.as_bytes())];
         writer.write_record(&vcf_record)?;
     }
